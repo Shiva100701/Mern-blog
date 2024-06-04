96c8dbf5a821023759bbecd3e33801d5ecc8a422
ShivamUponly <shivam.mishra@uponly.in>
2024-06-05 01:04:41 +0530

    add post section to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
+import DashPosts from '../components/DashPosts'
 
 
 
@@ -25,6 +26,7 @@ function Dashboard() {
         <DashSidebar />
       </div>
       {tab==='profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts />}
     </div>
   ) 
 }
